02d4ef1daf64b470ee714d8ad7a7333591b647e0
Cam <camskidmore2000@gmail.com>
2021-05-16 15:23:23 -0700

    Created add file and compiled it for linux executable.

---
+++ add.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "Enter the first number: ";
+	int num1;
+	cin >> num1;
+	cout << endl << "Enter the second number: ";
+	int num2;
+	cin >> num2;
+
+	int add;
+	add = num1 + num2;
+
+	cout << "The added number is: " << add << "!" << endl;
+	return 0;
+}
+
