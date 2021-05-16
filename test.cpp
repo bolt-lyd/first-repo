6f0de632bc92c73de1b9ddf92b04d2d4a493defe
Cam <camskidmore2000@gmail.com>
2021-05-16 15:14:43 -0700

    Added user name input

--- test.cpp
+++ test.cpp
@@ -1,10 +1,18 @@
 #include <stdio>
 #include <iostream>
+#include <strings>
 
 using namespace std;
 
 int main(){
-	cout << "Hello World!";
+	cout << "Hello World!" << endl;
+
+	cout << "What is your name?" << endl;
+	string name;
+
+	cin >> name;
+
+	cout << "Hello " << name << "!" << endl;
 
 	return 0;
 }
